fb4a985ae9d1286e9ac3ce3d63592458ac710b51
Max <maxcatalina@iMac-Max.local>
2024-05-16 12:02:51 +0300

    w

--- main.js
+++ main.js
@@ -1,66 +1,32 @@
-class SuperMath {
-	check(obj) {
-		const { X, Y, Z } = obj;
-		if (this.isInvalidInput(X, Y, Z)) {
-			alert('Введено некоректні дані. Спробуйте ще раз.');
-			this.input();
-			return;
-		}
-		const proceed = confirm(
-			`Чи ви хочете зробити дію ${Z} з числами ${X} і ${Y}?`
-		);
-		if (proceed) {
-			if (this.isValidOperator(Z)) {
-				const result = this.calculate(X, Y, Z);
-				alert(`Результат: ${result}`);
-			} else {
-				alert('Введено некоректний знак операції. Спробуйте ще раз.');
-				this.input();
+function Boo() {
+	let state = 0;
+
+	return {
+		next: () => {
+			state++;
+			switch (state) {
+				case 1:
+					console.log('start');
+					return { value: 1, done: false };
+				case 2:
+					return { value: 2, done: false };
+				case 3:
+					return { value: 3, done: false };
+				case 4:
+					console.log('finish');
+					return { value: undefined, done: true };
+				default:
+					return { value: undefined, done: true };
 			}
-		} else {
-			this.input();
-		}
-	}
-
-	input() {
-		const X = parseFloat(prompt('Введіть число X:'));
-		const Y = parseFloat(prompt('Введіть число Y:'));
-		const Z = prompt('Введіть знак операції (+ - / * %):');
-		this.check({ X, Y, Z });
-	}
-
-	isValidOperator(Z) {
-		return ['+', '-', '/', '*', '%'].includes(Z);
-	}
-
-	isInvalidInput(X, Y, Z) {
-		return (
-			isNaN(X) ||
-			isNaN(Y) ||
-			Z.trim() === '' ||
-			X === null ||
-			Y === null ||
-			Z === null
-		);
-	}
-
-	calculate(X, Y, Z) {
-		switch (Z) {
-			case '+':
-				return X + Y;
-			case '-':
-				return X - Y;
-			case '/':
-				return X / Y;
-			case '*':
-				return X * Y;
-			case '%':
-				return X % Y;
-			default:
-				throw new Error('Некоректний знак операції.');
-		}
-	}
+		},
+	};
 }
 
-const math = new SuperMath();
-math.check({ X: 14, Y: 3, Z: '*' });
+const iterator = Boo();
+console.log(iterator.next().value);
+console.log(iterator.next().done);
+console.log(iterator.next().value);
+console.log(iterator.next().done);
+console.log(iterator.next().value);
+console.log(iterator.next().done);
+console.log(iterator.next().done);
